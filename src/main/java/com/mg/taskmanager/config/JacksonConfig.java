2ff6c23d4e8457cf4612eb0d2b904c4464462ddd
MiguelGalvez81 <miguel_galvez81@hotmail.com>
2026-03-31 00:50:03 -0600

    Fix Jackson config

---
+++ src/main/java/com/mg/taskmanager/config/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.mg.taskmanager.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        // Deshabilitar características de serialización que causan excepciones
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.disable(SerializationFeature.INDENT_OUTPUT);
+        
+        return mapper;
+    }
+}
